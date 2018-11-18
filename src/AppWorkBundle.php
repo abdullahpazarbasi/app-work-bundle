4045930c74c09c3134352efedeb3e6ba8c1063aa
abdullah <mapazarbasi@hotmail.com>
2018-11-19 00:21:50 +0300

    Bundle created

---
+++ src/AppWorkBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright (c) 2018.
+ */
+/**
+ * User: abdullah
+ * Date: 18.11.2018
+ * Time: 23:23
+ */
+namespace AppWorkBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AppWorkBundle extends Bundle
+{
+}
